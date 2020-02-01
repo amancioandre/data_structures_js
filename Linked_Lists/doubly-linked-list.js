a5814bd3acf151d20e74a8960f11a58fab66904b
Andre Moraes <amancioandre@gmail.com>
2020-02-01 19:33:08 -0300

    :construction: Add DoublyLinkedList

---
+++ Linked_Lists/doubly-linked-list.js
@@ -0,0 +1,203 @@
+const BaseNode = require("../Utils/node");
+
+class Node extends BaseNode {
+  constructor(value) {
+    super(value);
+    this.previous = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const node = new Node(value);
+
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      console.log(this.tail, node);
+      this.tail.next = node;
+      node.previous = this.tail;
+      this.tail = node;
+    }
+
+    this.length += 1;
+
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return;
+
+    let tailNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.previous;
+      this.tail.next = null;
+
+      this.length -= 1;
+      tailNode.previous = null;
+    }
+
+    return tailNode;
+  }
+
+  shift() {
+    if (!this.head) return;
+
+    let headNode = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.previous = null;
+
+      headNode.next = null;
+    }
+
+    this.length -= 1;
+
+    return headNode;
+  }
+
+  unshift(value) {
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.head.previous = node;
+      node.next = this.head;
+      this.head = node;
+    }
+
+    this.length += 1;
+
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index > this.length) return;
+
+    let term;
+    let i;
+    let factor;
+    let currentNode;
+
+    if (index <= this.length / 2) {
+      term = "next";
+      i = 0;
+      factor = 1;
+      currentNode = this.head;
+    } else {
+      term = "previous";
+      i = this.length - 1;
+      factor = -1;
+      currentNode = this.tail;
+    }
+
+    while (i !== index) {
+      currentNode = currentNode[term];
+      i += factor;
+    }
+
+    return currentNode;
+  }
+
+  set(index, value) {
+    const node = this.get(index);
+
+    if (!node) return false;
+
+    node.value = value;
+
+    return true;
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      this.unshift(value);
+    } else if (index >= this.length - 1) {
+      this.push(value);
+    } else {
+      const currentNode = this.get(index);
+
+      if (!currentNode) return false;
+
+      const node = new Node(value);
+
+      const previousNode = currentNode.previous;
+
+      previousNode.next = node;
+      currentNode.previous = node;
+
+      node.next = currentNode;
+      node.previous = previousNode;
+    }
+    return true;
+  }
+
+  remove(index) {
+    if (index === 0) {
+      this.shift();
+    } else if (index === this.length - 1) {
+      this.pop();
+    } else {
+      const node = this.get(index);
+
+      if (!node) return false;
+
+      const previousNode = node.previous;
+      const nextNode = node.next;
+
+      previousNode.next = nextNode;
+      nextNode.previous = previousNode;
+    }
+
+    return true;
+  }
+
+  print() {
+    const arr = [];
+
+    let current = this.head;
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+
+    return arr;
+  }
+}
+
+const list = new DoublyLinkedList();
+console.log(list.push(0));
+console.log(list.push(1));
+console.log(list.push(2));
+
+// console.log(list.pop());
+// console.log(list);
+
+console.log(list.unshift(-1));
+console.log(list.unshift(-2));
+
+console.log(list.get(2));
+
+console.log(list.print());
+
+console.log(list.remove(-9));
+
+console.log(list.print());
+
+// console.log(list);
--- Linked_Lists/singly-linked-list.js
+++ Linked_Lists/singly-linked-list.js
@@ -77,15 +77,97 @@ class SinglyLinkedList {
   }
 
   get(index) {
-    if (!this.head || this.length - 1 < index) return;
+    if (!this.head || index >= this.length || index < 0) return;
 
     let i = 0;
     let current = this.head;
     while (i < index) {
       current = current.next;
+      i += 1;
     }
 
     return current;
+  }
+
+  set(index, value) {
+    let currentNode = this.get(index);
+
+    if (!currentNode) return false;
+
+    currentNode.value = value;
+
+    return true;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(value);
+    if (index === this.lenght - 1) return !!this.push(value);
+
+    let previousNode = this.get(index - 1);
+
+    if (!previousNode) return false;
+
+    let aftNode = previousNode.next;
+    const node = new Node(value);
+
+    node.next = aftNode;
+    previousNode.next = node;
+
+    this.length += 1;
+
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.shift();
+    if (index === this.lenght - 1) return !!this.pop();
+
+    let previousNode = this.get(index - 1);
+
+    if (!previousNode) return false;
+
+    let removedNode = previousNode.next;
+
+    previousNode.next = removedNode.next;
+
+    this.length -= 1;
+
+    return removedNode;
+  }
+
+  reverse() {
+    if (this.length <= 1) return;
+
+    let currentNode = this.head;
+    this.head = this.tail;
+    this.tail = currentNode;
+
+    let nextNode;
+    let previousNode = null;
+
+    while (currentNode) {
+      nextNode = currentNode.next;
+      currentNode.next = previousNode;
+      previousNode = currentNode;
+      currentNode = nextNode;
+    }
+
+    return this;
+  }
+
+  print() {
+    const arr = [];
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return arr;
   }
 }
 
@@ -98,4 +180,6 @@ list.push(2);
 list.unshift(-1);
 list.unshift(-2);
 
-console.log(list);
+console.log(list.print());
+console.log(list.reverse());
+console.log(list.print());
